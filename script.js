1e2fa017ca10329be7798e701461941396f8bcda
pietrafn <71992173+Pietrafn@users.noreply.github.com>
2022-10-05 20:21:13 -0300

    tirei o comentário do javascript

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/*colocar uma if pra caso tema == claro, document.getElementById("mudaTema"). value = "Tema escuro"  else document.getElementById("mudaTema"). value = "tema claro"
-onde está escrito conquistas, vou mudar para linguagens, vou colocar quais linguagens de programação eu sei, e quais pretendo aprender*/
 tema();
 function mudaTema() {
   document.body.classList.toggle("dark");
